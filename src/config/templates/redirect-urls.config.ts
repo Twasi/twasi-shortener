c9d045528f3d225813aaee7c8ca8288a1d34819c
DieserMerlin <email@merlinw.de>
2020-09-03 16:28:21 +0200

    + Not yet working graphql-subscription implementation

--- src/config/templates/redirect-urls.config.ts
+++ src/config/templates/redirect-urls.config.ts
@@ -7,7 +7,7 @@ export type RedirectUrlsConfig = {
 
 export const DefaultRedirectUrlsConfig: RedirectUrlsConfig = {
     allowedUrls: {
-        regex: new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gi).source,
+        regex: new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/).source,
         flags: "gi"
     }
 }
