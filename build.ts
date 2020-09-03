d623e98d1b1046c95ca4aa4a69e51622c0fcc197
Merlin Westphal <email@merlinw.de>
2020-09-03 08:03:33 +0200

    Optimized build.ts

--- build.ts
+++ build.ts
@@ -1,5 +1,8 @@
-import {existsSync} from "fs";
+import {existsSync, rmdirSync} from "fs";
 import {execSync} from "child_process";
+import {ArgumentParser} from "merlins-argument-parser";
+
+const args = new ArgumentParser(process.argv.slice(2));
 
 console.log('Checking backend:')
 
@@ -10,9 +13,13 @@ if (existsSync('node_modules')) {
     console.log('Backend-dependencies installed.');
 }
 
-if (existsSync('dist') && existsSync('dist/index.js')) {
+const buildBackend = args.get('rebuild').asBool() || args.get('rebuild-backend').asBool();
+if (existsSync('dist') && existsSync('dist/index.js') && !buildBackend) {
     console.log('"dist/index.js" exists. Skipping compilation.');
 } else {
+    if (existsSync('dist')) {
+        rmdirSync('dist', {recursive: true});
+    }
     execSync('npm run build', {stdio: 'inherit'});
     console.log('Backend built to "./dist".');
 }
@@ -20,20 +27,32 @@ if (existsSync('dist') && existsSync('dist/index.js')) {
 console.log('Checking frontend:');
 
 if (!existsSync('frontend')) {
-    console.log('Frontend folder missing. Please make sure to clone with "--recursive" flag.')
+    console.log('Frontend folder missing. Please make sure to clone with "--recursive" flag or create empty frontend folder.')
     process.exit(0);
 }
 
+if (!existsSync('frontend/package.json')) {
+    console.log('Frontend package.json missing. Recloning frontend...');
+    execSync('git clone https://github.com/twasi/twasi-shortener-frontend.git .', {
+        stdio: 'inherit',
+        cwd: './frontend'
+    });
+}
+
 if (existsSync('frontend/node_modules')) {
-    console.log('"node_modules"-folder exists. Skipping installation.')
+    console.log('"node_modules"-folder exists. Skipping installation.');
 } else {
     execSync('npm i', {stdio: 'inherit', cwd: './frontend'});
     console.log('Frontend-dependencies installed.');
 }
 
-if (existsSync('frontend/build') && existsSync('frontend/build/index.html')) {
+const buildFrontend = args.get('rebuild').asBool() || args.get('rebuild-frontend').asBool();
+if (existsSync('frontend/build') && existsSync('frontend/build/index.html') && !buildFrontend) {
     console.log('"frontend/build/index.html" exists. Skipping compilation.')
 } else {
+    if (existsSync('frontend/build')) {
+        rmdirSync('frontend/build', {recursive: true});
+    }
     execSync('npm run build', {stdio: 'inherit', cwd: './frontend'});
     console.log('Frontend built to "./frontend/build".')
 }
