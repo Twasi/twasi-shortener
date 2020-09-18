bad840423abe32c7cf92502b00da0b3058cd9c9f
Merlin Westphal <email@merlinw.de>
2020-09-18 12:47:51 +0200

    + canUseShort query

--- src/webserver/controllers/graphql/public/shorts-query.controller.ts
+++ src/webserver/controllers/graphql/public/shorts-query.controller.ts
@@ -2,11 +2,13 @@ import {gql} from "apollo-server-express";
 import {GraphQLController} from "../../include";
 import {ShortsConfig} from "../../../../config/app-config";
 import {ApolloContext} from "../../../webserver";
+import {canUserUseShort} from "../../../../routines/urls/url-creation-permission-checks.routine";
 
 export const ShortsQueryController: GraphQLController = {
     typeDefs: [
         gql`extend type Query {
             myShorts: [String]!,
+            canUseShort(short: String!): Boolean!,
             defaultPublicShort: String!,
             defaultAuthenticatedShort: String!
         }`
@@ -19,6 +21,10 @@ export const ShortsQueryController: GraphQLController = {
                     const shorts = [ShortsConfig.public, ShortsConfig.panel];
                     // TODO add mappings
                     return shorts;
+                },
+                canUseShort: async (source, args, context: ApolloContext) => {
+                    if (!context.authorization) return ShortsConfig.public === args.short;
+                    return canUserUseShort(context.authorization, args.short);
                 },
                 defaultPublicShort: () => ShortsConfig.public,
                 defaultAuthenticatedShort: () => ShortsConfig.panel,
