a7f5b63ded98dfe332fa52060b03da84b416c980
DieserMerlin <email@merlinw.de>
2020-09-21 14:29:33 +0200

    + Redirect to confirmation page when classification is negative

--- src/webserver/controllers/rest/redirection.controller.ts
+++ src/webserver/controllers/rest/redirection.controller.ts
@@ -2,13 +2,14 @@ import {RestController} from "../include";
 import {Router} from "express";
 import {DBShortenedUrlModel} from "../../../database/schemas/shortened-url.schema";
 import {publishUrlUpdate} from "../graphql/public/public-stats.controller";
+import {DBShortenedUrl} from "../../../models/shortened-url.model";
 
-async function findRedirection(short: string, tag: string): Promise<string | null> {
+async function findRedirection(short: string, tag: string): Promise<DBShortenedUrl | null> {
     const result = await DBShortenedUrlModel.findOne({short, tag});
     if (result) {
         typeof result.hits === "number" ? result.hits++ : result.hits = 0;
         result.save().then(() => publishUrlUpdate(result).then());
-        return result.redirection;
+        return result;
     }
     return null;
 }
@@ -17,7 +18,11 @@ const router = Router();
 router.get('/:short/:tag', async (req, res) => {
     const redirection = await findRedirection(req.params.short, req.params.tag);
     if (redirection) {
-        res.redirect(redirection);
+        res.redirect(
+            redirection.classification === false ?
+                '/?confirmRedirection=' + encodeURIComponent(redirection.redirection) :
+                redirection.redirection
+        );
     } else res.redirect('/?404');
 });
 
