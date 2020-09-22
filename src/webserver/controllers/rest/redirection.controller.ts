4ea42940c4ae98b7c4924ad8d96a206fa95dec04
DieserMerlin <email@merlinw.de>
2020-09-22 16:01:56 +0200

    + Test URL before redirection

--- src/webserver/controllers/rest/redirection.controller.ts
+++ src/webserver/controllers/rest/redirection.controller.ts
@@ -3,6 +3,7 @@ import {Router} from "express";
 import {DBShortenedUrlModel} from "../../../database/schemas/shortened-url.schema";
 import {publishUrlUpdate} from "../graphql/public/public-stats.controller";
 import {DBShortenedUrl} from "../../../models/urls/shortened-url.model";
+import {testUrl} from "../../../routines/urls/tests/test-url.routine";
 
 async function findRedirection(short: string, tag: string): Promise<DBShortenedUrl | null> {
     const result = await DBShortenedUrlModel.findOne({short, tag});
@@ -18,8 +19,9 @@ const router = Router();
 router.get('/:short/:tag', async (req, res) => {
     const redirection = await findRedirection(req.params.short, req.params.tag);
     if (redirection) {
+        const {result} = await testUrl(redirection.redirection);
         res.redirect(
-            redirection.classification === false ?
+            redirection.classification === false || result.CRITICAL ?
                 '/?confirmRedirection=' + encodeURIComponent(redirection.redirection) :
                 redirection.redirection
         );
